d6d4720af64f0f48c7361a6cdf034636e8fac079
wtfAbduraim <rahmonkulovabduraim16@gmail.com>
2024-03-19 17:39:22 +0300

    1.3

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,28 @@ import Img2 from "./Img/Img.svg";
 
 function App() {
   return (
-    <div className="container max-w-[1440px] w-full flex m-auto max-md:flex-col items-center justify-around">
+    <div className="container max-w-[1440px] flex w-full m-auto sm:flex sm:items-center">
       <img
-        className="max-w-[720px] max-h-[899px]  w-full h-full "
+        className="max-w-[720px] max-h-[899px] w-full h-full"
         src={Img}
         alt="img"
       />
-      <div className="h-full md:bg-white p-10 w-96 max-md:absolute px-10 max-sm:mb-52 max-xs:mb-20">
+      <div className="h-full md:bg-white p-10 xs:w-96 absolute px-10 w-80 sm:relative">
         <p className="text-xs text-midnight font-roboto font-normal">
           Welcome back
         </p>
-        <h1 className="text-3xl text-black font-roboto font-bold mb-8 max-xs:text-2xl max-ss:mb-3">
+        <h1 className="xs:text-3xl text-black font-roboto font-bold xs:mb-8 text-2xl mb-3">
           Login to your account
         </h1>
 
         <label class="block ">
-          <p className="text-xs font-roboto font-normal mb-2 text-grey">
+          <p className="text-xs font-roboto font-normal mb-1 text-grey">
             Email
           </p>
           <input
             type="email"
             name="email"
-            class="font-roboto w-80 max-xs:w-64 mt-2 px-3 py-2 bg-white border border-slate-200 placeholder-slate-900 focus:outline-none focus:border-sky-100 focus:ring-sky-800 block rounded-md sm:text-sm focus:ring-1 max-md:bg-transparent"
+            class="font-roboto ss:w-80 w-64 mt-1 px-3 py-1 bg-white border border-slate-200 placeholder-slate-900 focus:outline-none focus:border-sky-100 focus:ring-sky-800 block rounded-md sm:text-sm focus:ring-1 bg-transparent"
             placeholder="John.snow@gmail.com"
           />
         </label>
@@ -36,26 +36,26 @@ function App() {
           <input
             type="password"
             name="email"
-            class="font-roboto w-80 max-xs:w-64 mt-2 px-3 py-2 bg-white border border-slate-200 placeholder-slate-900 focus:outline-none focus:border-sky-100 focus:ring-sky-800 block rounded-md sm:text-sm focus:ring-1 max-md:bg-transparent"
+            class="font-roboto ss:w-80 w-64 mt-2 px-3 py-1 bg-white border border-slate-200 placeholder-slate-900 focus:outline-none focus:border-sky-100 focus:ring-sky-800 block rounded-md sm:text-sm focus:ring-1 max-md:bg-transparent"
             placeholder="********"
           />
         </label>
 
-        <div className="flex justify-between mt-10 items-center	max-ss:mt-3">
+        <div className="flex justify-between xs:mt-10 items-center	mt-1">
           <div className="flex items-center	">
             <input type="checkbox" class="checked:bg-blue bg-grey" />
-            <p className="ml-2 text-midnight text-sm font-normal font-roboto max-md:text-white">
+            <p className="ml-2 sm:text-midnight text-sm font-normal font-roboto text-white">
               Remember me
             </p>
           </div>
           <a
-            className="text-sm font-normal text-darkBlue font-roboto max-md:text-white"
+            className="text-sm font-normal sm:text-darkBlue font-roboto text-white"
             href="#"
           >
             Forgot password?
           </a>
         </div>
-        <div className="text-center mt-10 max-ss:mt-4">
+        <div className="text-center xs:mt-10 mt-1">
           <button className="font-bold text-center bg-green rounded-md w-80 max-xs:w-64 max-xs:mr-12 p-3 max-xs:p-1 text-white mb-3">
             Login now
           </button>
@@ -70,11 +70,11 @@ function App() {
             Or sign-in with google
           </button>
         </div>
-        <div className="flex items-center mt-20 max-xs:mt-5">
-          <p className="font-normal font-roboto text-grey max-md:text-white">
+        <div className="flex items-center xs:mt-20 mt-5">
+          <p className="font-normal text-xs font-roboto sm:text-grey text-white">
             Dont have an account?
           </p>
-          <a className="ml-2 font-roboto font-normal text-blue" href="#">
+          <a className="ml-2 font-roboto text-xs text-blue" href="#">
             Join free today
           </a>
         </div>
